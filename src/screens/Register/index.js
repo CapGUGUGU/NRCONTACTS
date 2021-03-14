e52aafa5f37edbef56d2ff2dee7c79c2ce3041cb
Yuanhao Li <1204538104@qq.com>
2021-03-14 11:31:48 +1030

    2

--- src/screens/Register/index.js
+++ src/screens/Register/index.js
@@ -1,30 +1,32 @@
 import React from 'react';
-import {Text, View} from 'react-native';
 import {useState} from 'react/cjs/react.development';
 import RegisterComponent from '../../components/SignUp';
-import env from '../../config/env';
+import axiosInstance from '../../helpers/axiosInterceptor';
 
 const Register = () => {
   const [form, setForm] = useState({});
   const [errors, setErrors] = useState({});
-  const BACKEND_URL = env;
-  console.log('BACKEND:>> ', BACKEND_URL);
-  console.log('__DEV__:>> ', __DEV__);
+
+  React.useEffect(() => {
+    axiosInstance.post('/contacts/').catch(err => {
+      console.log('Error:>> ', err.response);
+    }); 
+  }, []);
 
   const onChange = ({name, value}) => {
     setForm({...form, [name]: value});
     if (value !== '') {
       if (name === 'password' && value.length < 6) {
-        setErrors((prev) => {
+        setErrors(prev => {
           return {...prev, [name]: 'Password need min 6 characters'};
         });
       } else {
-        setErrors((prev) => {
+        setErrors(prev => {
           return {...prev, [name]: null};
         });
       }
     } else {
-      setErrors((prev) => {
+      setErrors(prev => {
         return {...prev, [name]: 'This field is required'};
       });
     }
@@ -32,27 +34,27 @@ const Register = () => {
 
   const onSubmit = () => {
     if (!form.userName) {
-      setErrors((prev) => {
+      setErrors(prev => {
         return {...prev, userName: 'Please add Username'};
       });
     }
     if (!form.firstName) {
-      setErrors((prev) => {
+      setErrors(prev => {
         return {...prev, firstName: 'Please add First Name'};
       });
     }
     if (!form.lastName) {
-      setErrors((prev) => {
+      setErrors(prev => {
         return {...prev, lastName: 'Please add Last Name'};
       });
     }
     if (!form.email) {
-      setErrors((prev) => {
+      setErrors(prev => {
         return {...prev, email: 'Please add Email'};
       });
     }
     if (!form.password) {
-      setErrors((prev) => {
+      setErrors(prev => {
         return {...prev, password: 'Please add Password'};
       });
     }
